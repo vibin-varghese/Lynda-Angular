c0f5db515e3402c92658a417eb159e93ad6f6c8a
Vibin Varghese <vvarghe2@jaguarlandrover.com>
2017-09-20 14:25:50 +0100

    comment added

--- Lynda-Angular/WebContent/js/controllers.js
+++ Lynda-Angular/WebContent/js/controllers.js
@@ -8,7 +8,7 @@ artistControllers.controller('ListController', ['$scope', '$http', function ($sc
   });
 }]);
 
-
+//comment
 artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams) {
 	  $http.get('js/data.json').success(function (data) {
 
